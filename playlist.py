3632d355381c86450103a4e812794da5450be11e
Noah K. Tilton <binaural@gmail.com>
2010-06-16 13:29:21 -0500

    some py

--- playlist.py
+++
@@ -1,6 +0,0 @@
-class Playlist:
-
-    def __init__(self,dirs):
-        self.dirs = dirs
-        print dirs
-
--- riddim.py
+++ riddim.py
@@ -1,61 +1,36 @@
+import sys
 import socket
-import struct
+import threading
 
-import playlist
 
-class Jukebox:
+from lib.options import RiddimOptions
+from lib.client import RiddimClient
+from lib.server import RiddimServer
 
-    def __init__(self):
-        pass
 
-    def modes(self):
-        # repeat
-        # continue
-        # shuffle
-    
 
-class Riddim:
+if __name__ == '__main__':
+    riddim_options = RiddimOptions()
+    if riddim_options.signal:
+        if riddim_options.signal == "start":
+            print """
+                RiDDiM v0.1
+            """
 
-    def __init__(self,port=18944):
-        self.hostname = socket.gethostname()
+            riddim_server = RiddimServer(('localhost',18449)).server
+            ip, port = riddim_server.server_address
 
-        self.CHUNK_SIZE=8192
-        print "riddim running on %s" % self.port
+            t = threading.Thread(target=riddim_server.serve_forever)
+            t.setDaemon(not riddim_options.foreground)
+            t.start()
 
-    def header(self):
-        return """ICY 200 OK
-icy-notice1: <BR>Riddim<BR>
-icy-notice2: riddim-server<BR>
-icy-name: riddim on %s
-icy-genre: unknown
-icy-url: http://github.com/noah/riddim
-content-type: audio/mpeg
-icy-pub: 1
-icy-br: 128
-icy-metaint: %s
+            print "Server started at http://localhost:%s" % port
 
-""" % (self.hostname,self.CHUNK_SIZE)
+        # if riddim_options.signal == "stop":
+        #     s = xmlrpclib.ServerProxy('http://localhost:18944')
+        #     s.shutdown()
 
-    def on(self):
-        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.sockets.bind(('localhost',self.port))
-        self.socket.listen(1)
-        self.conn,self.addr = self.sockets.accept()
-        print 'connect by', self.addr
-
-    def off(self):
-        self.conn.close()
-
-    def stream(self):
-        while 1:
-            with open('mp3/asdf.mp3') as f:
-              while bytes is not None:
-                bytes = f.read(self.CHUNK_SIZE)
-                self.conn.send(bytes)
-
-playlist = Playlist(['./mp3'])
-
-riddim = Riddim(18945)
-riddim.on()
-riddim.stream(playlist)
-riddim.off()
+    #else:
+    #    if riddim_options.flag:
+    #        #print "running %s flag" % riddim_options.flag
+    #        sys.exit(0)
