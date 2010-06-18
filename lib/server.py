04ec0333bc11a5c54ba41f4aa5947fbd281ac244
Noah K. Tilton <binaural@gmail.com>
2010-06-17 22:18:56 -0500

    streaming to squeezebox works; not with 127.0.0.1 (?)

--- lib/server.py
+++ lib/server.py
@@ -57,6 +57,15 @@ class RiddimServerRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         # Pragma: no-cache,rate=1.000000,stream-time=0,stream-offset=0:0,request-context=1,max-duration=0
         # Connection: Close
 
+        """ squeezebox (wut?) """
+        # Connection: close
+        # Cache-Control: no-cache
+        # Accept: */*
+        # Host: localhost:18944
+        # User-Agent: iTunes/4.7.1 (Linux; N; Linux; i686-linux; EN; utf8) SqueezeCenter, Squeezebox Server/7.4.1/28947
+        # Icy-Metadata: 1
+
+
         H = self.headers
         legit_client = False
         try:
@@ -64,11 +73,15 @@ class RiddimServerRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         except KeyError, e:
             print "Key not found:  %s" % e
 
+        user_agent = None
         if legit_client:
             try:
-                print "User-Agent:  %s" % H['user-agent']
+                user_agent = H['user-agent']
             except KeyError, e:
                 print "Couldn't get user agent!"
+
+        if user_agent:
+            print "User-Agent:  %s" % user_agent
 
         self.do_HEAD()
         # playlist = RiddimPlaylist()
