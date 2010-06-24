935f7e36a04b6be76d099b70a13b026c6f5c790d
noah <noah@downbe.at>
2010-06-24 16:36:11 -0500

    fixed streaming

--- lib/streamer.py
+++ lib/streamer.py
@@ -6,10 +6,12 @@ import socket
 from lib.mp3 import RiddimMP3
 from lib.data import RiddimData
 from lib.config import RiddimConfig
+from lib.playlist import RiddimPlaylist
 
 class RiddimStreamer(object):
     def __init__(self,request):
         self.data = RiddimData()
+        self.playlist = RiddimPlaylist()
         self.config = RiddimConfig(os.getcwd()).config
         self.request = request
         self.byte_count = 0
@@ -36,64 +38,48 @@ class RiddimStreamer(object):
         else:
             return '\x00'
 
-    def get_song(self):
-        playlist = sorted(eval(self.data['playlist']).keys())
-        from pprint import pprint 
-        pprint(playlist)
-        if playlist is None:
-            print "Playlist empty"
-            return
-        I = int(self.data['index'])
-        if I is None: I = 0
+    def stream(self,icy_client=False):
+        while True:
+            song = self.playlist.get_song()
+            if not song: return
+            self.mp3 = RiddimMP3(song)
+            print 'streaming %s' % self.mp3
 
-        try:
-            path = playlist[I]
-        except IndexError:
-            print "No song at index %s" % I
-            return
-
-        self.data['status'] = 'playing'
-        self.data['song'] = self.mp3
-        self.data['index'] = str(I)
-        idx = int(self.data['index'])
-        self.data['index'] = idx + 1
-        self.data['status'] = 'stopped'
-        self.data['song'] = ''
-
-    def stream(self,path,icy_client=False):
-        self.mp3 = RiddimMP3(path)
-        print 'streaming %s' % self.mp3
-
-        try:
-            # this loop gets its ideas about the shoutcast protocol from amarok
-            buffer              = 0
-            buffer_size         = 4096
-            metadata_interval   = self.config.getint('icy','metaint')
-
-            f = file(path, 'r')
-            f.seek(self.mp3.start())
-            self.dirty_meta = True
-            mp3_size = self.mp3.size()
-            while f.tell() < mp3_size:
-                bytes_until_meta = (metadata_interval - self.byte_count)
-                if bytes_until_meta == 0:
-                    if icy_client:
-                        self.request.send(self.get_meta())
-                    self.byte_count = 0
-                else:
-                    if bytes_until_meta < buffer_size:
-                        n_bytes = bytes_until_meta
+            try:
+                # this loop gets its ideas about the shoutcast protocol from amarok
+                buffer              = 0
+                buffer_size         = 4096
+                metadata_interval   = self.config.getint('icy','metaint')
+                f = file(song, 'r')
+                f.seek(self.mp3.start())
+                self.dirty_meta = True
+                mp3_size = self.mp3.size()
+                while f.tell() < mp3_size:
+                    # TODO
+                    # put check for next, prev etc flags here
+                    bytes_until_meta = (metadata_interval - self.byte_count)
+                    if bytes_until_meta == 0:
+                        if icy_client:
+                            self.request.send(self.get_meta())
+                        self.byte_count = 0
                     else:
-                        n_bytes = buffer_size
+                        if bytes_until_meta < buffer_size:
+                            n_bytes = bytes_until_meta
+                        else:
+                            n_bytes = buffer_size
+                        buffer = f.read(n_bytes)
+                        self.request.send(buffer)
+                        self.byte_count += len(buffer)
+                self.dirty_meta = True
+                self.data['index'] = int(self.data['index']) + 1
+            except IOError, e:
+                self.data['status'] = 'stopped'
+                self.data['song'] = None
+                if e.errno == errno.EPIPE:
+                    print "Broken pipe"
+                elif e.errno == errno.ECONNRESET:
+                    print "Connection reset by peer"
+                else:
+                    print errno.errorcode[e.errno]
+                break # while
 
-                    buffer = f.read(n_bytes)
-                    self.request.send(buffer)
-                    self.byte_count += len(buffer)
-            self.dirty_meta = True
-        except IOError, e:
-            if e.errno == errno.EPIPE:
-                print "Broken pipe"
-            elif e.errno == errno.ECONNRESET:
-                print "Connection reset by peer"
-            else:
-                print errno.errorcode[e.errno]
