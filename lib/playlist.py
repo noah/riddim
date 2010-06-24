717c2c075c754bb6965dcf4b925a4293cbe09715
noah <noah@downbe.at>
2010-06-24 16:35:40 -0500

    playlist class

--- lib/playlist.py
+++ lib/playlist.py
@@ -1,6 +1,28 @@
-class Playlist:
+from lib.data import RiddimData
 
-    def __init__(self,dirs):
-        self.dirs = dirs
-        print dirs
+class RiddimPlaylist(object):
 
+    def __init__(self):
+        self.data = RiddimData()
+        self.playlist = sorted(eval(self.data['playlist']).keys())
+
+    def get_song(self):
+        playlist = eval(self.data['playlist']).keys()
+        if playlist is None: return False
+        I = int(self.data['index'])
+        if I is None: I = 0
+    
+        try:
+            song = playlist[I]
+            self.data['status'] = 'playing'
+            self.data['song'] = song
+            return song
+        except IndexError:
+            print "No song at index %s" % I
+            return False
+    
+        # self.data['index'] = str(I)
+        # idx = int(self.data['index'])
+        # self.data['index'] = idx + 1
+        # self.data['status'] = 'stopped'
+        # self.data['song'] = ''
