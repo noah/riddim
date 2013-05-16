f072fce8bbd0824ff9e2e5e18ec1a4ed86b8d01f
noah <noahktilton@gmail.com>
2013-05-16 16:22:26 -0500

    clean

--- riddim.py
+++ riddim.py
@@ -9,39 +9,37 @@ from lib.control import Control
 from lib.playlist import Playlist
 
 # needed if LANG=en_US.UTF-8
-sys.stdout = codecs.getwriter('utf8')( sys.stdout )
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 if __name__ == "__main__":
 
     args = Args()
 
     if args.signal:
-        {
-                "start"     : Control().start,
-                "stop"      : Control().stop,
-                "restart"   : Control().restart,
-        }[ args.signal ]()
+        c = Control(args.port)
+        if   args.signal == "start"     : c.start()
+        elif args.signal == "stop"      : c.stop()
+        elif args.signal == "restart"   : c.restart()
 
     else:
-        playlist = Playlist()
-        if args.query:
-            print playlist.query()
-        elif args.shuffle:
-            print playlist.shuffle()
-        elif args.repeat:
-            print playlist.repeat()
-        elif args.kontinue:
-            print playlist.kontinue()
+        playlist = Playlist(args.port)
+        if   args.query                 : print playlist.query()
+        elif args.shuffle               : print playlist.shuffle()
+        elif args.repeat                : print playlist.repeat()
+        elif args.kontinue              : print playlist.kontinue()
+        elif args.next_album            : print playlist.next_album()
+        elif args.next_artist           : print playlist.next_artist()
         else:
             for action, arg in args.args_dict.items():
+                try:
                     print {
                         "clear"         : playlist.clear,
                         "index"         : playlist.index,
-                        "enqueue"       : playlist.enqueue,
+                        "enqueue"       : playlist.enqueue
                     }[ action ]( arg )
+                except: pass
 
-    try:    sys.stdout.close()
-    except: pass
-    try:    sys.stderr.close()
-    except: pass
-    sys.exit(0)
+    try:        sys.stdout.close()
+    except:     pass
+    try:        sys.stderr.close()
+    except:     pass
